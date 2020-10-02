2dcabd5ce4a667c95916d15e30aee181fd0ec481
Tony Xhepa <tonyxhepa2020@gmail.com>
2020-10-02 12:41:59 +0200

    first commit

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="container mx-auto" id="app">
+    <admin-index></admin-index>
+</div>
+@endsection
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <link rel="stylesheet" href="https://unpkg.com/vue-multiselect@2.1.0/dist/vue-multiselect.min.css">
+
+    <!-- Styles -->
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}" defer></script>
+</head>
+<body class="font-sans antialiased">
+<div class="min-h-screen bg-gray-100">
+   @yield('content')
+</div>
+</body>
+</html>
